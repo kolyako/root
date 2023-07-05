f3d8188d0f023bce8366a42d0a6fa6b398db0703
kolyako <138399788+kolyako@users.noreply.github.com>
2023-07-05 15:28:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+/*const string = 'hello'
+const number = 10
+const boolean = true
+const object = {}
+const nullVar = null
+const undefineVar = undefined
+
+const result = number  + object;
+
+console.log(typeof result)
+
+console.log(Number(""))
+console.log(Number("string"))
+console.log(Number({}))
+console.log(Number(nullVar))
+console.log(Number(undefineVar))
+console.log(Number(true))
+console.log(Number(false))
+console.log(Number([3, 2]))
+
+console.log(Number(23))
+console.log(Number(-23))
+
+const comporetion = (a) => {
+    if (a > 0){
+        return 'it is true'
+    } else {
+        return 'you dont folse'
+    }
+}
+
+const resault = comporetion(-20);
+
+console.log(resault);*/
+
+const namen = 'Tirion';
+console.log(namen.toUpperCase()); // => TIRION
+console.log(namen);
